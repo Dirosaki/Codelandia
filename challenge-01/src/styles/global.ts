6155f3e68da8c9ceddefc3eb85b7fe66e1713eef
Dirosaki <diegosgomes27@gmail.com>
2024-01-23 05:19:12 -0300

    Project configuration

---
+++ challenge-01/src/styles/global.ts
@@ -0,0 +1,50 @@
+import { createGlobalStyle, css } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle(
+  ({ theme }) => css`
+    *,
+    *::before,
+    *::after {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      outline: transparent;
+    }
+
+    html {
+      height: 100%;
+    }
+
+    body,
+    #root {
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      background: ${theme.colors.blue[50]};
+    }
+
+    body,
+    input,
+    textarea,
+    select,
+    button {
+      font-family: 'Inter', sans-serif;
+      font-size: 1rem;
+      font-weight: 500;
+    }
+
+    button {
+      border: none;
+      cursor: pointer;
+    }
+
+    a {
+      text-decoration: none;
+      color: currentColor;
+    }
+
+    li {
+      list-style: none;
+    }
+  `
+)
---
+++ challenge-01/src/styles/theme.ts
@@ -0,0 +1,13 @@
+export const theme = {
+  colors: {
+    brand: '#E07B67',
+    dark: {
+      10: '#121214',
+      20: '#17171A',
+      30: '#202024',
+      40: '#252529',
+      50: '#AFABB6',
+      60: '#FFFFFF',
+    },
+  },
+} as const
