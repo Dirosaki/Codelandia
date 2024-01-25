39ae7cd30bc1d5a31a708c34c77ef84b6e86dc5d
Dirosaki <diegosgomes27@gmail.com>
2024-01-25 20:20:24 -0300

    Feat: Home page

--- challenge-02/src/App.svelte
+++ challenge-02/src/App.svelte
@@ -1,5 +1,29 @@
-<script lang="ts">
+<script>
   import Header from 'components/Header.svelte'
+  import Product from 'components/Product.svelte'
+
+  import products from 'assets/data/tennis.json'
 </script>
 
 <Header />
+<main class="px-6 py-14 md:px-8 lg:py-20 lg:flex">
+  <div class="flex flex-col items-center lg:flex-1 lg:max-w-[1200px] lg:mx-auto">
+    <strong class="text-xl font-medium text-black font-archivo md:text-2xl lg:text-[32px]"
+      >Os melhores em um só lugar
+    </strong>
+    <p
+      class="mt-3 text-sm font-normal text-center text-black max-w-96 font-archivo md:text-base lg:mt-6 lg:text-2xl lg:max-w-[600px]"
+    >
+      A marca Jordan na JordanShoes é a escolha certa para os amantes de sneakers que
+      buscam estilo e conforto.
+    </p>
+
+    <ul
+      class="flex flex-col w-full grid-cols-2 gap-10 mt-10 md:grid md:gap-x-8 md:gap-y-10 lg:grid-cols-3"
+    >
+      {#each products as { image, name, model, price }}
+        <Product {image} {name} {model} {price} />
+      {/each}
+    </ul>
+  </div>
+</main>
---
+++ challenge-02/src/components/Product.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  export let image: string = ''
+  export let name: string = ''
+  export let model: string = ''
+  export let price: number = 0
+
+  const formattedPrice = new Intl.NumberFormat('pt-br', {
+    style: 'currency',
+    currency: 'BRL'
+  }).format(price)
+</script>
+
+<li class="flex flex-col">
+  <div class="flex justify-center py-8 bg-ghost-white">
+    <img src={image} alt={name} class="max-w-[225px]" />
+  </div>
+  <strong class="mt-6 text-base font-medium text-black font-archivo md:text-lg">
+    {name}
+  </strong>
+  <span class="mt-3 text-base font-medium text-brand font-archivo">{model}</span>
+  <strong class="mt-6 text-xl font-medium text-black font-space">{formattedPrice}</strong>
+</li>
